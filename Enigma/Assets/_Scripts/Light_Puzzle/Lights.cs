51dd11ae8006aabf7a198a87d344d99cee6eb32a
ClementLSW <clementlsw@gmail.com>
2023-03-01 18:48:27 +0800

    Implemented light puzzle

---
+++ Enigma/Assets/_Scripts/Light_Puzzle/Lights.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lights : MonoBehaviour
+{
+
+    private SpriteRenderer sr;
+    private LightPuzzle lp;
+
+    [SerializeField] private Sprite lit;
+    [SerializeField] private Sprite unlit;
+    [SerializeField] private Lights prev, next;
+    [SerializeField] private bool isLit;
+    public bool Lit { get { return isLit; } }
+
+
+    private void Awake() {
+        sr = GetComponent<SpriteRenderer>();
+        lp = GetComponentInParent<LightPuzzle>();
+
+        //if (isLit) sr.sprite = lit;
+        //else sr.sprite = unlit;
+
+        if (isLit) sr.color = Color.yellow;
+        else sr.color = Color.white;
+    }
+
+    public void SendSignal() {
+        this.toggle();
+        if(prev) prev.toggle();
+        if(next) next.toggle();
+    }
+
+    private void toggle() {
+        isLit = !isLit;
+
+        if (isLit) {
+            //sr.sprite = lit;
+            sr.color = Color.yellow;
+            lp.Increment();
+        }
+        else {
+            //sr.sprite = unlit;
+            sr.color = Color.white;
+            lp.Decrement();
+        }
+
+    }
+}
